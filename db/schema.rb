1d367377b6994d03d449839467f9af69f99e73fa
yukina.nakashima <ykiinna.76@gmail.com>
2021-06-13 14:07:26 +0000

    CRUD tasks

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2021_06_13_105502) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "title"
+    t.string "status"
   end
 
 end
